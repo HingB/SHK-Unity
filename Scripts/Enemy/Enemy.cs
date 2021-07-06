a28b5cd5c4b58e717634c978e8c4d2cca7ff17e3
HingB <50633061+HingB@users.noreply.github.com>
2021-07-06 15:47:24 +0400

    Refactor project

---
+++ Scripts/Enemy/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _movementSquareMultyplayer;
+
+    private Vector3 _target;
+
+    public event UnityAction<Enemy> Died;
+
+    void Start()
+    {
+        DetermineNewTarget();
+    }
+
+    void Update()
+    {
+        Move();
+        if (transform.position == _target)
+            DetermineNewTarget();
+    }
+
+    private void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+    }
+
+    private void DetermineNewTarget()
+    {
+        _target = Random.insideUnitCircle * _movementSquareMultyplayer;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.TryGetComponent(out Player player))
+            Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        Died?.Invoke(this);
+    }
+}
---
+++ Scripts/UI/FinalPanelOpener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalPanelOpener : MonoBehaviour
+{
+    [SerializeField] private GameObject _finalPanel;
+    [SerializeField] private EnemyCounter _enemyCounter;
+
+    private void OnEnable()
+    {
+        _enemyCounter.AllEnemiesDied += OnAllEnemiesDied;
+    }
+
+    private void OnDisable()
+    {
+        _enemyCounter.AllEnemiesDied -= OnAllEnemiesDied;
+    }
+
+    private void OnAllEnemiesDied()
+    {
+        _finalPanel.SetActive(true);
+    }
+}
